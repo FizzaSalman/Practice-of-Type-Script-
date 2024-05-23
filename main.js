07a30e6025e67baf2de6eb4bcc68ba64447dd5f2
FizzaSalman <159646126+FizzaSalman@users.noreply.github.com>
2024-05-23 21:16:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,203 @@
+"use strict";
+//console.log("Hello World");
+Object.defineProperty(exports, "__esModule", { value: true });
+//variables....
+//code k ander kisi data ko store krne k liye we use variable
+//let x = "fizza"
+//console.log(x);
+//data types...
+//let myName: string = "fizza";
+//let age: number = 20;
+//console.log(age);
+//operators
+//1. arithmatic means plus
+//let num1: number = 10
+//let num2: number =20
+//console.log(num1 + num2)
+//2. assignment operators 
+//let fname: string = "fizza";
+//console.log(fname);
+//let x = 20;
+//console.log(x);
+//additional asigning operator
+//let x = 20;
+//let y = 5;
+//let result = x + y
+//console.log(result)
+//comaprision operator
+//let y = 25
+//
+//console.log( y == 20)
+// IF ELSE STATEMENT
+//let student = "8th"
+//if(student == "8th") {
+//   console.log("get in")
+//} else{
+//  console.log("get out")
+//}
+//let age1: number = 16
+//if(age1 >= 18){
+//  console.log("come to party")
+//}else{
+//  console.log("sorry u r unnder age cant come")
+//}
+//let age = 17
+//if(age >= 18){
+//  console.log('you can drive')
+//}else{
+//console.log('you cannot drive')
+//}
+//let fname: string = "ahmed"
+//let age: number = 18
+//if(fname === "ahmed" && age === 18){
+//  console.log("you login")
+//}else{
+//  console.log("login failed")
+//}
+//const username = "fizza"
+//const password = 123
+//if(username === "fizza" && password === 123){
+//  console.log("login successful")
+//}else{
+//  console.log("login failed")
+//}
+//LOGICAL OR OPERATOR////is me aik condition true hogi tau code run hjyga
+//let email = "fizzasalman@gmail.com";
+//let password = 123123
+//if(email == "fizzasalman@gmail.com" || password == 1231244){
+//  console.log("login successful")
+//}else{
+//  console.log("login failed")
+//
+//conditional ternary operator///isko variable m store hota hai if else m nhi
+//isko multiple time n likh skte 
+//let age = 21;
+//let result = (age >= 18)? "you r eligible for party":"you r not eligible"
+//console.log(result)
+//FUNCTION with parameters n arguement
+//function myName(){
+//  let myName = "fizza"
+//let myAge = 30
+//console.log(`my name is ${myName} & my age is ${myAge}`)
+//}
+//myName()
+//function student(myName:string){
+//  console.log(`my name is: ${myName}`)
+//}
+//student("fizza")
+//student("salman")
+//student("ayoub")
+//student("abdullah")
+//student("zayan")
+//function sum(num1:number,num2:number){
+//  console.log(num1 + num2)//isi tarha hum subtract,multiply sare arithmetic opertaion use ho skte h
+//}
+//sum(5,3)
+//mr malik result
+//physics 40
+//mathematics 50
+//chemistry 60
+//eng 60
+//function result(physics:number,math:number,chemistry:number,eng:number){
+//  console.log(physics+math+chemistry+eng)
+//}
+//result(40,50,60,60)
+//array
+//let Pakteam = ["Babar","Rizwan","Shaheen","Shadab","Hassan"]
+//pop method kisi bhi array se last element ko remove kerdeta h
+//pop method jis element ko delete kerta h usi ko return kerta h deleted elemnt ko return kerta h
+//Pakteam.pop()
+//console.log(Pakteam)
+//let sub = ["physics","chemisry","eng","urdu"]
+//let newSub = sub.pop()
+//console.log(newSub)
+//let message = "hello world"
+//console.log(message)
+// 001 json object (JAVA SCRIPT OBJECT NOTATtion)ki pehchan key r value dono doble cots m aygi
+//let obj = {
+//name:"noor",
+//age:16,
+//developer:true
+//}
+//console.log(JSON.stringify(obj))
+//let Bio = {
+//name:"fizza",
+//age:30,
+//teacher:true,
+// hobby:true
+//}
+//console.log(JSON.stringify(Bio))
+//syntax error basically spelling mistake is a syntax error
+//lett message:string = "samiya"
+//console.log(message)
+//TYPE ERROR
+//let message:string = "samiya"
+//console.loger(message)
+//assignbility error
+//let student = "fizza"
+//student = 30
+//let giaic_Student: string = "fizza"
+//let giaic_Student: age = 18
+//intersection
+//inter face me obj k bd = nhi ayga
+//interface obj {
+//name:string,
+//age:number
+//}
+//let obj = 
+//{
+//name:"ahmed",
+//age:22
+//}
+//console.log(obj.name)
+//type inference
+//let student = "mehwish";
+//student = "samiya"
+//console.log(student)
+// use const where value do not change
+//const value_Gravity = 9.8;
+//value_Gravity = 10//error ayga qk uper gravity ki value cinst m di h
+//const student_Giaic = "Maheen"
+//student_Giaic = "Bushra"
+//const a = 5;//
+//const b : number = 33// strong typing
+//const c = "best"//type inference
+//ARRAY k ander hum num string boolean storekerskte hain array ki b data type define hoti h
+//let pak_Team: string[] = ["Babar","shaheen","Kamram","Umer","hassan"]
+//pak_Team.pop()//kisi b array se last wala element ko remove kerdeta h
+//console.log(pak_Team)
+//let subjects: string[] = ["eng","math","urdu","sci","isl"]
+//subjects.push("pak stdies")//last me element add hojata hai
+//console.log(subjects)
+let sub = ["eng", "urdu", "math", "bio"];
+sub.shift(); //first wala element remove hoga
+console.log(sub);
+let colors = ["red", "yellow", "green",];
+colors.unshift("purple"); ///fst me element add hoga
+console.log(colors);
+//3.comparision operator
+// ==
+// ===
+// >
+// <
+// !=
+// >=
+// <=
+//function
+//function potato() {
+//let veg = "potato"
+//  console.log(`Ahmed plz bring ${veg}`)
+//}
+//potato()
+//object
+//type kid = {
+//  name:string,
+//experience:string,
+//age:number
+//}
+//let kid = {
+//  name:"mudassir",
+// experience:"student",
+//age:9
+//}
+//console.log(kid.name)
